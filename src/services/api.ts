4406f3487ccb63f2b1a1e4c2ac5df1ed8cd628af
AlexanderRoncalli <42423882+AlexRoncalli@users.noreply.github.com>
2025-10-16 03:43:34 -0300

    correção do endereço da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Troque isso:
-  // baseURL: 'http://localhost:3333',
-
-  // Por isso:
-  baseURL: import.meta.env.VITE_API_URL, 
+  //         👇👇👇 CORRIJA ESTA LINHA 👇👇👇
+  baseURL: 'https://gerenciador-estoque-api.onrender.com'
 });
 
 export default api;
